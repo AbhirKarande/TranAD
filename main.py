51b30529ffc58ab2db55fcb206679f5016bc9108
Abhir Karande <abhir511@gmail.com>
2024-02-19 22:18:43 -0500

    working testing phase

--- main.py
+++ main.py
@@ -34,13 +34,11 @@ def load_dataset(dataset):
 		if dataset == 'MSL': file = 'C-1_' + file
 		if dataset == 'UCR': file = '136_' + file
 		if dataset == 'NAB': file = 'ec2_request_latency_system_failure_' + file
-		loader.append(np.load(os.path.join(folder, f'{file}.npy'), allow_pickle=True))
+		loader.append(np.load(os.path.join(folder, f'{file}.npy')))
 	# loader = [i[:, debug:debug+1] for i in loader]
 	if args.less: loader[0] = cut_array(0.2, loader[0])
-	print(loader[0].shape, loader[1].shape, loader[2].shape)	
-
-	train_loader = DataLoader(loader[0]) 
-	test_loader = DataLoader(loader[1])
+	train_loader = DataLoader(loader[0], batch_size=loader[0].shape[0])
+	test_loader = DataLoader(loader[1], batch_size=loader[1].shape[0])
 	labels = loader[2]
 	return train_loader, test_loader, labels
 
@@ -56,7 +54,6 @@ def save_model(model, optimizer, scheduler, epoch, accuracy_list):
         'accuracy_list': accuracy_list}, file_path)
 
 def load_model(modelname, dims):
-	print('dims', dims)
 	import src.models
 	model_class = getattr(src.models, modelname)
 	model = model_class(dims).double()
@@ -297,18 +294,15 @@ def backprop(epoch, model, data, dataO, optimizer, scheduler, training = True):
 
 if __name__ == '__main__':
 	train_loader, test_loader, labels = load_dataset(args.dataset)
-	print(type(train_loader), type(test_loader), type(labels))
 	if args.model in ['MERLIN']:
 		eval(f'run_{args.model.lower()}(test_loader, labels, args.dataset)')
 	model, optimizer, scheduler, epoch, accuracy_list = load_model(args.model, labels.shape[1])
 
 	## Prepare data
 	trainD, testD = next(iter(train_loader)), next(iter(test_loader))
-	print("before", trainD.shape, testD.shape)
 	trainO, testO = trainD, testD
 	if model.name in ['Attention', 'DAGMM', 'USAD', 'MSCRED', 'CAE_M', 'GDN', 'MTAD_GAT', 'MAD_GAN'] or 'TranAD' in model.name: 
 		trainD, testD = convert_to_windows(trainD, model), convert_to_windows(testD, model)
-	print(trainD.shape, testD.shape, trainO.shape, testO.shape)
 
 	### Training phase
 	if not args.test:
@@ -319,7 +313,7 @@ if __name__ == '__main__':
 			accuracy_list.append((lossT, lr))
 		print(color.BOLD+'Training time: '+"{:10.4f}".format(time()-start)+' s'+color.ENDC)
 		save_model(model, optimizer, scheduler, e, accuracy_list)
-	#	plot_accuracies(accuracy_list, f'{args.model}_{args.dataset}')
+		#plot_accuracies(accuracy_list, f'{args.model}_{args.dataset}')
 
 	### Testing phase
 	torch.zero_grad = True
@@ -327,46 +321,26 @@ if __name__ == '__main__':
 	print(f'{color.HEADER}Testing {args.model} on {args.dataset}{color.ENDC}')
 	loss, y_pred = backprop(0, model, testD, testO, optimizer, scheduler, training=False)
 
-	print('backprop for loss done')
-
-
-
-
-	# ### Plot curves
+	### Plot curves
 	# if not args.test:
 	# 	if 'TranAD' in model.name: testO = torch.roll(testO, 1, 0) 
-	# 	plotter(f'{args.model}_{args.dataset}', testO, y_pred, loss, labels)
+		#plotter(f'{args.model}_{args.dataset}', testO, y_pred, loss, labels)
 
 	### Scores
 	df = pd.DataFrame()
 	lossT, _ = backprop(0, model, trainD, trainO, optimizer, scheduler, training=False)
-	print('backprop for lossT done')
-
-	#allign labels with the batch size of 32
-
-	# for i in range(loss.shape[1]):
-	# 	lt, l, ls = lossT[:, i], loss[:, i], labels[:, i]
-	# 	result, pred = pot_eval(lt, l, ls); preds.append(pred)
-	# 	df = df.append(result, ignore_index=True)
+	for i in range(loss.shape[1]):
+		lt, l, ls = lossT[:, i], loss[:, i], labels[:, i]
+		result, pred = pot_eval(lt, l, ls); preds.append(pred)
+		df = df.append(result, ignore_index=True)
 	# preds = np.concatenate([i.reshape(-1, 1) + 0 for i in preds], axis=1)
 	# pd.DataFrame(preds, columns=[str(i) for i in range(10)]).to_csv('labels.csv')
 	lossTfinal, lossFinal = np.mean(lossT, axis=1), np.mean(loss, axis=1)
 	labelsFinal = (np.sum(labels, axis=1) >= 1) + 0
-	lossTfinalFinal = np.asarray(lossTfinal).astype('float32').reshape(-1, 1)
-	lossFinalFinal = np.asarray(lossFinal).astype("float32").reshape(-1, 1)
-	labelsFinalFinal = np.asarray(labelsFinal).astype("float32").reshape(-1, 1)
-	print('final stuff done')
-
-	print("shape", lossTfinalFinal.shape, lossFinalFinal.shape, labelsFinalFinal.shape)	
-
-
-
-	
-	result, _ = pot_eval(lossTfinalFinal, lossFinalFinal, labelsFinalFinal)
-	print('pot eval done')
+	result, _ = pot_eval(lossTfinal, lossFinal, labelsFinal)
 	result.update(hit_att(loss, labels))
 	result.update(ndcg(loss, labels))
-	#print(df)
+	print(df)
 	pprint(result)
 	# pprint(getresults2(df, result))
 	# beep(4)
